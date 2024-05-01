e009c60d3df93bb066e540352e7aa9c598790d9c
Andrey Maximov <hdsh138720@gmail.com>
2024-05-01 13:38:05 +0000

    add html page and work with the  js metod

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
 const test = "test";
+
+const array  = [1,10,20,2, 2,3,3,4,5,];
+
+const sumFunction = (arr) => {
+    let countSum = 0;
+for(let i = 0 ; i <arr.length ; i++ ){
+countSum += arr[i];
+}
+return countSum;
+}
+
+console.log(sumFunction(array));
+
+const sum  = array.reduce((arr , acc) => arr + acc, 45)
+console.log(sum);
+
+const sortArray  = array.sort((a , b ) => b - a)
+console.log(sortArray);
+
+const uniqueNumbers = Array.from(new Set(array));
+
+console.log(uniqueNumbers);
+
+const newUniqueNumbers  = array.filter((value , i, self) => self.indexOf(value) === i)
+
+const str = "andRey";
+
+
+console.log(Array.from(str.toUpperCase()));
